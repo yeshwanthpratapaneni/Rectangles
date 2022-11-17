8cbf39b720c72230c3e4b3878e595a8badc2951a
Yeshwanth  Pratapaneni ramamurthy naidu <yeshwanthpratapaneniramamurthynaidu@Yeshwanths-MacBook-Air.local>
2022-11-16 20:34:25 -0500

    Updated test cases

--- test/org/nuvalence/rectangles/RectangleServiceTest.java
+++ test/org/nuvalence/rectangles/RectangleServiceTest.java
@@ -36,39 +36,57 @@ public class RectangleServiceTest {
 
     @Test
     public void containmentTest() {
+
+        // Rectangle R1 Completely contains R2
         Rectangle r1 = buildRectangle(0, 7, 0,7);
         Rectangle r2 = buildRectangle(1,4,1,4);
 
         IntersectionResult result = rectangleService.compareRectangle(r1, r2);
         assertEquals(IntersectionType.CONTAINMENT, result.getIntersectionType());
         assertNull(result.getIntersectionPoints());
+        assertEquals(r1, result.getContainer());
+        assertEquals(r2, result.getContainee());
     }
 
     @Test
     public void adjacencyPartialTest() {
+        // Both R1 and R2 are adjacent, however only a partial side is adjacent
+        // There are 6 Intersection points in the following two rectangles
+
         Rectangle r1 = buildRectangle(0, 7, 0,7);
         Rectangle r2 = buildRectangle(7,12,2,9);
 
         IntersectionResult result = rectangleService.compareRectangle(r1, r2);
         assertEquals(IntersectionType.ADJACENT_PARTIAL, result.getIntersectionType());
+        assertEquals(6, result.getIntersectionPoints().size());
     }
 
     @Test
     public void adjacencySubLineTest() {
+        // Both R1 and R2 are adjacent, however one side of the rectangle is
+        // completely covered in the other's adjacent side hence this implies a adjacent sub line rectangle overlap
+        // 2 points of rectangle r2 is completely covered in the other rectangle r1
+
         Rectangle r1 = buildRectangle(0, 7, 0,7);
         Rectangle r2 = buildRectangle(7,15,2,3);
 
         IntersectionResult result = rectangleService.compareRectangle(r1, r2);
         assertEquals(IntersectionType.ADJACENT_SUB_LINE, result.getIntersectionType());
+        assertEquals(2, result.getIntersectionPoints().size());
     }
 
     @Test
     public void adjacencyProperTest() {
+        // Both R1 and R2 are adjacent and there is a complete overlap of the sides
+        // hence this implies a adjacent proper rectangle overlap
+        // 8 points of rectangle r1 and r2 are intersected on one of their sides
+
         Rectangle r1 = buildRectangle(0, 7, 0,7);
         Rectangle r2 = buildRectangle(7,15,0,7);
 
         IntersectionResult result = rectangleService.compareRectangle(r1, r2);
         assertEquals(IntersectionType.ADJACENT_PROPER, result.getIntersectionType());
+        assertEquals(8, result.getIntersectionPoints().size());
     }
 
     private Rectangle buildRectangle(int x1, int x2, int y1, int y2){
